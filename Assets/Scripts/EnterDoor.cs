27f65ec4f318a0b1c96e8849dce048a33f0cc201
soublaee <yarenkirca4@outlook.com>
2024-03-10 16:44:04 +0100

    test push

---
+++ Assets/Scripts/EnterDoor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject DoorOutside;
+    [SerializeField] private GameObject DoorInside;
+    private Vector3 PositionDoorOutside;
+    private Vector3 PositionDoorInside;
+
+    [SerializeField] bool inTrigger = false;
+    private void Start()
+    {
+        PositionDoorInside = DoorInside.transform.position;
+        PositionDoorOutside = DoorOutside.transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && inTrigger == true)
+        {
+            Debug.Log("DoorOutside is "+ PositionDoorOutside);
+            Debug.Log("DoorInside is"+ PositionDoorInside);
+            Debug.Log("pressed E in door");
+
+            Player.transform.position = PositionDoorOutside;
+            //teleport
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        inTrigger = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inTrigger = false;
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,56 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    public float downwardForce = 0f;
+
+    Animator animator;
 
     [SerializeField] private Rigidbody2D rb; //serialize field zorgt ervoor dat een private zoals een public kan gebruikt worden. k vindt t zelf extra hassle but sure lmfao
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal"); //movement
 
         if (Input.GetButtonDown("Jump") && IsGrounded()) //als je jump drukt dan..
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower); //jump je
+            animator.SetBool("isJumping", true);
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) //als je jump lang drukt
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f); //dan blijf je langer in de air
+            animator.SetBool("isJumping", true);
         }
 
-        //if (Input.GetButtonDown("goingDown") && rb.velocity.y > 0.5f) //als je jump lang drukt
-        //{
-            //Rigidbody2D.AddForce(transform.down * downwardForce, ForceMode.Impulse);
-        //}
+        if (Input.GetKeyDown(KeyCode.S) && rb.velocity.y > 0.5f) //als je jump lang drukt
+        {
+            rb.velocity = Vector2.down * downwardForce; //dan val je naar beneden
+        }
         Flip();
+
+        if (IsGrounded() == false) {
+            animator.SetBool("isJumping", false);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); //movement
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.4f, groundLayer);
     }
 
     private void Flip()

0a14ed8dad89586e701a8f4385466322be8c40cc
soublaee <yarenkirca4@outlook.com>
2024-03-16 17:56:11 +0100

    the main menu is REAL!!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
